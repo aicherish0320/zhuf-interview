bea26a94055efa05620f28a4ee5bf806886a49bc
aicherish0320 <aicherish0320@163.com>
2022-07-08 18:04:33 +0800

    v-for 中的 key

---
+++ vue2-examples/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <ForKeyVue></ForKeyVue>
+  </div>
+</template>
+<script>
+import ForKeyVue from './components/ForKey.vue'
+export default {
+  components: {
+    ForKeyVue
+  }
+}
+</script>
+<style lang="scss"></style>
---
+++ vue2-examples/src/components/ForKey.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="list in lists" :key="list.label">
+        {{ list.label }}
+        <input type="checkbox" :value="list.value" />
+      </li>
+    </ul>
+    <button @click="handleDeleteItem">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForKey',
+  data() {
+    return {
+      lists: [
+        {
+          label: 'VueJS',
+          value: false
+        },
+        {
+          label: 'ReactJS',
+          value: false
+        },
+        {
+          label: 'NodeJS',
+          value: false
+        }
+      ]
+    }
+  },
+  methods: {
+    handleDeleteItem() {
+      this.lists.shift()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
